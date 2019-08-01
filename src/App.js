6f88ca42acc55defbc3848519c1ca5610265dfcf
Sunil Kumar Banjare <banjare1990@gmail.com>
2019-08-01 16:37:59 +0530

    react-router and sass setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import Posts  from "./components/Posts";
 import PostForm  from "./components/PostForm";
-import './App.css';
+import './styles/App.scss';
+import Routing  from './components/Routing'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <PostForm/>
+        <Routing />
+        {/* <PostForm/>
         <hr />
-         <Posts />
+         <Posts /> */}
       </div>
     );
   }
---
+++ src/components/Routing.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Posts from './Posts'
+function Routing() {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/post">Posts</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <Route exact path="/" component={Home} />
+        <Route path="/post" component={Posts} />
+        <Route path="/about" component={About} />
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+export default Routing;
